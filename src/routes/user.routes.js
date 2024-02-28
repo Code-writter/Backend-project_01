866098d81cea924c30087512f42d5d5cf475103d
Abhishek Tiwari <142007480+Code-writter@users.noreply.github.com>
2024-02-28 22:26:26 +0530

    login and logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
@@ -19,4 +20,10 @@ userRouter.route("/register").post(
     registerUser
     )
 // router.post('/register',registerUser )
+
+userRouter.route('/login').post(loginUser)
+
+// Secured routes
+userRouter.route('/logout').post(verifyJWT ,logoutUser)
+
 export  {userRouter}
