c2ac5a06f9b5dbabcee4a230652bd1d150c92910
Abhishek Tiwari <142007480+Code-writter@users.noreply.github.com>
2024-03-03 20:36:18 +0530

    Subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -167,7 +167,7 @@ const logoutUser = asyncHandler(async (req, res) => {
     .clearCookie('refreshToken', options)
     .json(new ApiResponse(200, {}, "User logged out"))
 })
-// FIXME:
+
 const refreshAccessToken = asyncHandler(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
